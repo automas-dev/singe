05a0809318b94cc1869cd0a67d8e94d04ec355f1
Thomas Harrison <twh2898@vt.edu>
2020-09-12 22:29:23 -0400

    tmp

--- main.cpp
+++ main.cpp
@@ -261,8 +261,9 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    sf::Vector2i lastMouse (200, 200);
-    sf::Mouse::setPosition(lastMouse, window);
+    sf::Vector2f lastMouse (200, 200);
+    // sf::Vector2i lastMouse (200, 200);
+    // sf::Mouse::setPosition(lastMouse, window);
 
     Menu menu (font);
     menu.setTitle("Game");
@@ -271,7 +272,7 @@ int main() {
         menu.hide();
         window.setMouseCursorGrabbed(true);
         window.setMouseCursorVisible(false);
-        sf::Mouse::setPosition(lastMouse, window);
+        // sf::Mouse::setPosition(lastMouse, window);
     });
     menu.addMenuItem("Load", []() {});
     menu.addMenuItem("Options", []() {});
@@ -282,6 +283,8 @@ int main() {
     Camera cam;
     cam.setScreen(window.getSize());
     cam.move(3, 2, 1);
+
+    bool mouseReset = false;
 
     sf::Clock clock;
 
@@ -303,6 +306,15 @@ int main() {
                 break;
             case sf::Event::MouseMoved:
                 menu.onMouseMove(event.mouseMove);
+                if (!menu.isVisible && !mouseReset)
+                {
+                    sf::Vector2f mouse (event.mouseMove.x, event.mouseMove.y);
+                    sf::Vector2f delta (mouse.x - lastMouse.x, mouse.y - lastMouse.y);
+                    mouseReset = true;
+                    sf::Mouse::setPosition({lastMouse.x, lastMouse.y}, window);
+                    mouseReset = false;
+                    cam.rotate(delta.y, delta.x);
+                }
                 break;
             case sf::Event::MouseButtonPressed:
                 menu.onMouseDown(event.mouseButton);
@@ -328,17 +340,17 @@ int main() {
         // TODO: update
 
         if (!menu.isVisible) {
-            int z = sf::Keyboard::isKeyPressed(sf::Keyboard::S) - sf::Keyboard::isKeyPressed(sf::Keyboard::W);
             int x = sf::Keyboard::isKeyPressed(sf::Keyboard::D) - sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+            int z = sf::Keyboard::isKeyPressed(sf::Keyboard::S) - sf::Keyboard::isKeyPressed(sf::Keyboard::W);
             cam.move(x * 0.2, 0, z * 0.2);
         }
 
-        if (!menu.isVisible) {
-            auto pos = sf::Mouse::getPosition(window);
-            auto delta = pos - lastMouse;
-            cam.rotate(-delta.y / 2, -delta.x / 2);
-            sf::Mouse::setPosition(lastMouse, window);
-        }
+        // if (!menu.isVisible) {
+        //     auto pos = sf::Mouse::getPosition(window);
+        //     auto delta = pos - lastMouse;
+        //     cam.rotate(-delta.y / 2, -delta.x / 2);
+        //     sf::Mouse::setPosition(lastMouse, window);
+        // }
 
         // window.clear(dark);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
