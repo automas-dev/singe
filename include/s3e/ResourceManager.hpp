a809fe83375e1035ea57c078c394d0a5290ab454
Thomas Harrison <twh2898@vt.edu>
2020-11-21 19:58:01 -0500

    Added DefaultResourceManager, documented ResourceManager

--- include/s3e/ResourceManager.hpp
+++ include/s3e/ResourceManager.hpp
@@ -10,25 +10,105 @@
 
 namespace Tom::s3e {
 
+    /**
+     * Manages resources and provides relative paths to resources based on a
+     * root directory.
+     *
+     * Files can be accessed by using the `res://` prefix in the file path.
+     */
     class ResourceManager {
     private:
-        std::unordered_map<std::string, Texture::Ptr> textures;
-        std::string resRoot;
+        std::string rootPath;
 
     public:
+        /**
+         * Construct a ResourceManager with the current working directory as the
+         * resource root path.
+         */
         ResourceManager(void);
+
+        /**
+         * Construct a ResourceManager with `path` as the resource root path.
+         *
+         * @param path the root directory for resource paths
+         */
         ResourceManager(const std::string & path);
+
+        /**
+         * Destruct the ResourceManager. If the user overrides ResourceManager,
+         * all stored resources should be freed.
+         */
         virtual ~ResourceManager();
 
+        /**
+         * Set the resource root path.
+         *
+         * @param path the new root directory for resource paths
+         */
         void setResourcePath(const std::string & path);
 
+        /**
+         * Resolve the path to a resource. If `path` has the `res://` protocol
+         * as it's prefix, this function with return the path relative to the
+         * resource root path. If `path` is relative or absolute, then `path`
+         * will be returned without modification.
+         *
+         * @param path the resource path
+         *
+         * @return the resolved resource path
+         */
+        std::string resPath(const std::string & path) const;
+    };
+
+    /**
+     * An implementation of ResourceManager that has methods for loading
+     * Texture, Model and Shader resources.
+     */
+    class DefaultResourceManager : public ResourceManager {
+    private:
+        std::unordered_map<std::string, Texture::Ptr> textures;
+
+    public:
+        using ResourceManager::ResourceManager;
+
+        /**
+         * Load a Texture from it's given name when first loaded.
+         *
+         * @param name the name of the Texture
+         *
+         * @return a shared_ptr to a Texture
+         */
         Texture::Ptr loadTexture(const std::string & name);
+
+        /**
+         * Load a texture from `path` and cache it using `name` as it's key.
+         * If you later call loadTexture with the same name, the cached Texture
+         * will be returned.
+         *
+         * @param name the name of the Texture
+         * @param path the path to the Texture files
+         *
+         * @return a shared_ptr to a Texture
+         */
         Texture::Ptr loadTexture(const std::string & name, const std::string & path);
 
+        /**
+         * Load a Shader using `vertexPath` and `fragmentPath`.
+         *
+         * @param vertexPath the path to the vertex shader source
+         * @param fragmentPath the path to the fragment shader source
+         *
+         * @return a shard_ptr to a Shader
+         */
         Shader::Ptr loadShader(const std::string & vertexPath, const std::string & fragmentPath);
-        Model::Ptr loadModel(const std::string & path);
 
-    protected:
-        std::string resPath(const std::string & path) const;
+        /**
+         * Load a Model from `path`.
+         *
+         * @param path the path to the obj file
+         *
+         * @return a shared_ptr to a Model
+         */
+        Model::Ptr loadModel(const std::string & path);
     };
 }
--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -5,42 +5,45 @@ namespace fs = std::filesystem;
 namespace Tom::s3e {
     ResourceManager::ResourceManager() : ResourceManager("./") { }
 
-    ResourceManager::ResourceManager(const std::string & path) : resRoot(path) { }
+    ResourceManager::ResourceManager(const std::string & path) : rootPath(path) { }
 
     ResourceManager::~ResourceManager() { }
 
     void ResourceManager::setResourcePath(const std::string & path) {
-        resRoot = path;
-    }
-
-    Texture::Ptr ResourceManager::loadTexture(const std::string & name) {
-        if (textures.count(name) > 0)
-            return textures[name];
-        else
-            return nullptr;
-    }
-
-    Texture::Ptr ResourceManager::loadTexture(const std::string & name, const std::string & path) {
-        auto newTex = Texture::create(resPath(path));
-        if (newTex)
-            textures[name] = newTex;
-        return newTex;
-    }
-
-    Shader::Ptr ResourceManager::loadShader(const std::string & vertexPath, const std::string & fragmentPath) {
-        return Shader::create(resPath(vertexPath), resPath(fragmentPath));
-    }
-
-    Model::Ptr ResourceManager::loadModel(const std::string & path) {
-        return Model::create(resPath(path));
+        rootPath = path;
     }
 
     std::string ResourceManager::resPath(const std::string & path) const {
         auto resProtoPos = path.find("res://");
 
         if (resProtoPos ==  0)
-            return { fs::path(resRoot) / fs::path(path.substr(6)) };
+            return { fs::path(rootPath) / fs::path(path.substr(6)) };
         else
             return path;
     }
 }
+
+namespace Tom::s3e {
+    Texture::Ptr DefaultResourceManager::loadTexture(const std::string & name) {
+        if (textures.count(name) > 0)
+            return textures[name];
+        else
+            return nullptr;
+    }
+
+    Texture::Ptr DefaultResourceManager::loadTexture(const std::string & name, const std::string & path) {
+        auto newTex = Texture::create(resPath(path));
+        if (newTex)
+            textures[name] = newTex;
+        return newTex;
+    }
+
+    Shader::Ptr DefaultResourceManager::loadShader(const std::string & vertexPath, const std::string & fragmentPath) {
+        return Shader::create(resPath(vertexPath), resPath(fragmentPath));
+    }
+
+    Model::Ptr DefaultResourceManager::loadModel(const std::string & path) {
+        return Model::create(resPath(path));
+    }
+}
+
