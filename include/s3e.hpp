d79c14fff5fe8738e907302c989c0573153a774e
Thomas Harrison <twh2898@vt.edu>
2020-11-28 06:46:49 -0500

    Added FrameBuffer.hpp and Texture.hpp to s3e.hpp

--- include/s3e.hpp
+++ include/s3e.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "s3e/Camera.hpp"
+#include "s3e/FrameBuffer.hpp"
 #include "s3e/GameBase.hpp"
 #include "s3e/Material.hpp"
 #include "s3e/Menu.hpp"
 #include "s3e/Model.hpp"
 #include "s3e/ResourceManager.hpp"
 #include "s3e/Shader.hpp"
+#include "s3e/Texture.hpp"
 #include "s3e/Util.hpp"
 #include "s3e/VBO.hpp"
 #include "config.h"
