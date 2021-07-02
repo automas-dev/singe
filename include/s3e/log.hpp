b8c42742972cfefccf6fd894b4c5c09b58d382fc
Thomas Harrison <twh2898@vt.edu>
2021-07-02 01:12:33 -0400

    Added check for NDEBUG when setting default SPDLOG_ACTIVE_LEVEL

--- include/s3e/log.hpp
+++ include/s3e/log.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #ifndef SPDLOG_ACTIVE_LEVEL
+#ifdef NDEBUG
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO
+#else
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#endif
 #endif
 
 #include <spdlog/spdlog.h>
-
-#define log_general spdlog::get("general")
-
