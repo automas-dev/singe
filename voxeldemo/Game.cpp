0700b1bae784cae70b357c7df178f18a95be4af0
Thomas Harrison <twh2898@vt.edu>
2021-07-02 23:51:14 -0400

    Copied workspace to voxeldemo

---
+++ voxeldemo/Game.cpp
@@ -0,0 +1,165 @@
+#include "Game.hpp"
+#include <exception>
+#include <glm/gtc/noise.hpp>
+#include <spdlog/spdlog.h>
+
+
+static void getGlError() {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        SPDLOG_ERROR("glGetError() returned {}", err);
+    }
+}
+
+Game::Game(const sf::String & resPath) : GameBase(), resManager(resPath) { }
+
+Game::~Game() { }
+
+void GLAPIENTRY MessageCallback( GLenum source,
+                                 GLenum type,
+                                 GLuint id,
+                                 GLenum severity,
+                                 GLsizei length,
+                                 const GLchar *message,
+                                 const void *userParam ) {
+    SPDLOG_ERROR("GL CALLBACK: {} type = 0x{:x}, severity = 0x{:x}, message = {}",
+                 ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+                 type, severity, message );
+}
+
+bool Game::onCreate() {
+    // defautFont loaded from memory by GameBase
+    fps = std::make_shared<FPSDisplay>();
+    fps->setFont(uiFont);
+    fps->setRate(0.1f);
+
+    // During init, enable debug output
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
+
+    // Add menu buttons
+    menu->addMenuItem("Resume", [&]() {
+        menu->hide();
+        SetMouseGrab(true);
+    });
+    menu->addMenuItem("Exit", [&]() {
+        window->close();
+    });
+
+    // Initialize camera to look at origin
+    camera->move({-3, 2, -1});
+    camera->rotate({0, 110});
+    camera->setFov(70);
+
+    shader = resManager.loadShader("shader/shader.vert", "shader/shader.frag");
+    if (!shader)
+        return false;
+
+    devTexture = resManager.loadTexture("devTexture", "img/uv.png");
+    if (!devTexture)
+        return false;
+    devTexture->setFilter(GL_LINEAR, GL_LINEAR_MIPMAP_LINEAR);
+
+    for (int x = 0; x < SubChunk::N; x++) {
+        for (int z = 0; z < SubChunk::N; z++) {
+            float u1 = x / (float)SubChunk::N;
+            float u2 = (x + 1) / (float)SubChunk::N;
+            float v1 = z / (float)SubChunk::N;
+            float v2 = (z + 1) / (float)SubChunk::N;
+            UV uv(u1, v1, u2, v2);
+            styles.push_back(std::make_shared<BlockStyle>(uv, uv, uv, uv, uv, uv));
+        }
+    }
+
+    chunks = std::make_shared<ChunkManager>();
+    float s = 0.02;
+    for (int x = 0; x < 100; x++) {
+        for (int z = 0; z < 100; z++) {
+            auto height = 10 + glm::simplex(glm::vec2(x * s, z * s)) * 4;
+            for (int y = 0; y < height; y++) {
+                chunks->set(x, y, z, styles[(x * 8 + z) % styles.size()]);
+            }
+        }
+    }
+    SPDLOG_DEBUG("To the model");
+    model = std::make_shared<Model>();
+    bool res = model->loadFromPoints(chunks->toPoints());
+    if (!res)
+        return false;
+
+    SetMouseGrab(true);
+    getGlError();
+    return true;
+}
+
+void Game::onDestroy() { }
+
+void Game::onKeyPressed(const sf::Event::KeyEvent & e) {
+    GameBase::onKeyPressed(e);
+}
+
+void Game::onKeyReleased(const sf::Event::KeyEvent & e) {
+    GameBase::onKeyReleased(e);
+}
+
+void Game::onMouseMove(const sf::Event::MouseMoveEvent & e) {
+    GameBase::onMouseMove(e);
+}
+
+void Game::onMouseDown(const sf::Event::MouseButtonEvent & e) {
+    GameBase::onMouseDown(e);
+}
+
+void Game::onMouseUp(const sf::Event::MouseButtonEvent & e) {
+    GameBase::onMouseUp(e);
+}
+
+void Game::onResized(const sf::Event::SizeEvent & e) {
+    GameBase::onResized(e);
+}
+
+void Game::onUpdate(const sf::Time & delta) {
+    float deltaS = delta.asSeconds();
+    fps->update(delta);
+
+    chunks->update(delta);
+}
+
+void Game::onDraw() const {
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glPointSize(10);
+
+    glm::mat4 vp = camera->projMatrix() * camera->viewMatrix();
+
+    shader->bind();
+    devTexture->bind();
+    shader->setMat4("mvp", vp);
+    shader->setMat4("model", model->modelMatrix());
+    {
+        model->draw();
+
+        glBegin(GL_POINTS);
+        {
+            glVertex3d(0, 0, 0);
+            glVertex3d(1, 0, 0);
+            glVertex3d(0, 1, 0);
+            glVertex3d(0, 0, 1);
+            glVertex3d(-0.5, 0, 0);
+            //glVertex3d(0, 0, -0.5);
+        }
+        glEnd();
+    }
+    shader->unbind();
+    devTexture->unbind();
+
+
+    window->pushGLStates();
+    window->draw(*fps);
+    window->popGLStates();
+}
+
---
+++ voxeldemo/Game.hpp
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <GL/glew.h>
+#include <SFML/OpenGL.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
+#include <memory>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <random>
+#include <s3e.hpp>
+using namespace Tom::s3e;
+
+struct ChunkGenerator {
+    int seed;
+    float scale;
+    BlockStyle::Ptr style;
+
+    typedef std::shared_ptr<ChunkGenerator> Ptr;
+    typedef std::shared_ptr<const ChunkGenerator> ConstPtr;
+
+    ChunkGenerator(void) : ChunkGenerator(std::rand()) { }
+
+    ChunkGenerator(int seed) : seed(seed), scale(0.05), style(std::make_shared<BlockStyle>()) { }
+
+    Chunk::Ptr loadChunk(int x, int z) {
+        auto chunk = std::make_shared<Chunk>(glm::vec3(x, 0, z));
+        for (int i = x; i < x + SubChunk::N; i++) {
+            for (int j = z; j < z + SubChunk::N; j++) {
+                auto height = glm::simplex(glm::vec3(x * scale, seed, z * scale));
+                height = 10 + height * 256;
+                for (int y = 0; y < height; y++) {
+                    chunk->set(i, y, j, style);
+                }
+            }
+        }
+        return chunk;
+    }
+};
+
+struct ChunkLoader {
+    std::thread t;
+    ChunkGenerator::Ptr gen;
+    std::list<std::pair<int, int>> loadQueue;
+    std::list<Chunk::Ptr> loaded;
+    std::mutex queueMutex;
+
+    typedef std::shared_ptr<ChunkLoader> Ptr;
+    typedef std::shared_ptr<const ChunkLoader> ConstPtr;
+
+    ChunkLoader(void) : t(&ChunkLoader::run, this) { }
+
+    ChunkLoader(const ChunkGenerator::Ptr & gen) : gen(gen) { }
+
+    void run(void) {
+    }
+
+    void load(int x, int z) {
+        auto pair = std::make_pair(x, z);
+        std::scoped_lock lock(queueMutex);
+        loadQueue.push_back(pair);
+    }
+};
+
+struct ChunkManager {
+    std::map<std::pair<int, int>, Chunk::Ptr> chunks;
+
+    typedef std::shared_ptr<ChunkManager> Ptr;
+    typedef std::shared_ptr<const ChunkManager> ConstPtr;
+
+    void set(int x, int y, int z, const BlockStyle::Ptr & style) {
+        get(x, y, z)->set(x, y, z, style);
+    }
+
+    Chunk::Ptr & get(int x, int y, int z) {
+        x = x / SubChunk::N;
+        z = z / SubChunk::N;
+        auto pair = std::make_pair(x, z);
+        if (chunks.count(pair) == 0) {
+            glm::vec3 pos(x * SubChunk::N, 0, z * SubChunk::N);
+            chunks[pair] = std::make_shared<Chunk>(pos);
+        }
+        return chunks[pair];
+    }
+
+    void update(const sf::Time & delta) {
+
+    }
+
+    std::vector<Vertex> toPoints(void) {
+        std::vector<Vertex> points;
+        for (auto & e : chunks) {
+            auto & chunk = e.second;
+            auto p = chunk->toPoints();
+            points.insert(points.end(), p.begin(), p.end());
+        }
+        return points;
+    }
+};
+
+class Game : public GameBase {
+    DefaultResourceManager resManager;
+    Shader::Ptr shader;
+    FPSDisplay::Ptr fps;
+    Texture::Ptr devTexture;
+    Model::Ptr model;
+    ChunkManager::Ptr chunks;
+    std::vector<BlockStyle::Ptr> styles;
+
+public:
+    Game(const sf::String & resPath);
+    virtual ~Game();
+
+    bool onCreate(void) override;
+    void onDestroy(void) override;
+
+    void onKeyPressed(const sf::Event::KeyEvent & e) override;
+    void onKeyReleased(const sf::Event::KeyEvent & e) override;
+    void onMouseMove(const sf::Event::MouseMoveEvent & e) override;
+    void onMouseDown(const sf::Event::MouseButtonEvent & e) override;
+    void onMouseUp(const sf::Event::MouseButtonEvent & e) override;
+    void onResized(const sf::Event::SizeEvent & e) override;
+
+    void onUpdate(const sf::Time & delta) override;
+    void onDraw(void) const override;
+};
