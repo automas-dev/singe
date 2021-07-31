ba9150644983b0365822468178c303dbf600dec3
Thomas Harrison <twh2898@vt.edu>
2021-07-31 15:48:41 -0400

    Added documentation to Physics

--- include/s3e/Core/Physics.hpp
+++ include/s3e/Core/Physics.hpp
@@ -5,10 +5,16 @@
 #include <mutex>
 #include <thread>
 
-#include "s3e/Support/log.hpp"
 #include "btBulletDynamicsCommon.h"
+#include "s3e/Support/log.hpp"
 
 namespace Tom::s3e {
+
+    /**
+     * Simulate physics and perform collision detection.
+     *
+     * This class processes physics and collisions on a seprate thread.
+     */
     class Physics {
         std::thread t;
         mutable std::mutex m;
@@ -17,6 +23,9 @@ namespace Tom::s3e {
         sf::Clock clock;
         float updateInterval;
 
+        /**
+         * The thread worker.
+         */
         void worker();
 
     public:
@@ -28,26 +37,82 @@ namespace Tom::s3e {
         btAlignedObjectArray<btCollisionShape *> collisionShapes;
 
     public:
+        /**
+         * Create a new Physics object and specify the update interval.
+         *
+         * @param updateInterval the time in seconds between each update.
+         */
         Physics(float updateInterval = 1. / 60);
 
+        /**
+         * Stop the thread and release resources from the world.
+         */
         virtual ~Physics();
 
+        /**
+         * Check if the Physics are being paused or running.
+         *
+         * @return are updates being processed
+         */
         bool getRunState() const;
 
+        /**
+         * Set the run state, true is running and false is paused.
+         *
+         * @param run the run state
+         */
         void setRunState(bool run);
 
+        /**
+         * Add a rigid body to the world.
+         *
+         * @param body the body to add to the world
+         */
         void addRigidBody(btRigidBody * body);
 
+        /**
+         * Create a new rigid body and add it to the world.
+         *
+         * The object will by static if the mass is set to 0.
+         *
+         * @param collisionShape the collision shape
+         * @param mass the object mass or 0 for static
+         * @param friction the object friction
+         *
+         * @return the created rigid body
+         */
         btRigidBody * makeRigidBody(btCollisionShape * collisionShape,
                                     btScalar mass = 0,
                                     btScalar friction = 0);
 
+        /**
+         * Step the simulation with delta time.
+         *
+         * If maxSubSteps is > 0 interpolation will be performed between steps.
+         *
+         * @param delta the step delta
+         * @param maxSubSteps the number of interpolations between steps
+         */
         void step(const sf::Time & delta, int maxSubSteps = 10);
 
+        /**
+         * Copy a transform for object i to trans.
+         *
+         * @param i the object index
+         * @param trans the btTransform to populate
+         */
         void getTransform(int i, btTransform & trans) const;
 
+        /**
+         * Get the collision object array as returned by dynamicsWorld.
+         *
+         * @return the collision object array
+         */
         btCollisionObjectArray & getCollisionObjectArray() const;
 
+        /**
+         * Remove all objects from the world.
+         */
         void removeObjects();
     };
 }
