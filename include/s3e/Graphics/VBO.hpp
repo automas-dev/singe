849781ae837aebd0a1f219c797953b45d7872ec7
Thomas Harrison <twh2898@vt.edu>
2021-07-31 21:45:46 -0400

    Added documentation to VBO, fixed VBO not initializing to 0 with empty constructor

--- include/s3e/Graphics/VBO.hpp
+++ include/s3e/Graphics/VBO.hpp
@@ -15,7 +15,7 @@ namespace Tom::s3e {
         glm::vec3 norm;
         glm::vec2 uv;
 
-        Vertex(void) {}
+        Vertex(void) : pos(0), norm(0), uv(0) {}
 
         Vertex(glm::vec3 pos, glm::vec3 norm, glm::vec2 uv)
             : pos(pos), norm(norm), uv(uv) {}
@@ -55,8 +55,14 @@ namespace Tom::s3e {
         }
     };
 
+    /**
+     * Manages a single vertex buffered object and vertex array object.
+     */
     class VBO {
     public:
+        /**
+         * OpenGL Draw mode.
+         */
         enum Mode {
             Lines = GL_LINES,
             LineStrip = GL_LINE_STRIP,
@@ -67,9 +73,15 @@ namespace Tom::s3e {
             TriangleFan = GL_TRIANGLE_FAN,
         };
 
+        /**
+         * OpenGL buffer usage.
+         */
         enum Usage {
+            /// Data will be buffered once and used multiple times
             Static = GL_STATIC_DRAW,
+            /// Data will be buffered and used a few times
             Stream = GL_STREAM_DRAW,
+            /// Data will be buffered and used once
             Dynamic = GL_DYNAMIC_DRAW,
         };
 
@@ -84,21 +96,65 @@ namespace Tom::s3e {
         typedef std::shared_ptr<VBO> Ptr;
         typedef std::shared_ptr<const VBO> ConstPtr;
 
+        /**
+         * Create a new VBO with mode and usage.
+         *
+         * @param mode the OpenGL draw mode
+         * @param usage the OpenGL buffer usage
+         */
         VBO(Mode mode = Mode::Triangles, Usage usage = Usage::Static);
+
+        /**
+         * Free OpenGL buffers.
+         */
         virtual ~VBO();
 
+        /**
+         * Get the OpenGL draw mode.
+         *
+         * @return the VBO::Mode
+         */
         Mode getMode();
 
+        /**
+         * Set the OpenGL draw mode.
+         *
+         * @param mode the new mode
+         */
         void setMode(Mode mode);
 
+        /**
+         * Get the OpenGL buffer usage.
+         *
+         * @return the VBO::Usage
+         */
         Usage getUsage();
 
+        /**
+         * Set the OpenGL buffer usage.
+         *
+         * @param usage the new usage
+         */
         void setUsage(Usage usage);
 
+        /**
+         * Load buffer with data from points.
+         *
+         * @param points the data to send to the buffer
+         */
         bool loadFromPoints(const std::vector<Vertex> & points);
 
+        /**
+         * Load buffer with data from points.
+         *
+         * @param points the data to send to the buffer
+         * @param n the numebr of points
+         */
         bool loadFromPoints(const Vertex * points, size_t n);
 
+        /**
+         * Bind the vao, then draw, then unbind the vao.
+         */
         void draw() const;
     };
 }
