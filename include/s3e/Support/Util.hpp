63c49f854d1c9e9d60a6a69e8fe2a9ee2a2dda1d
Thomas Harrison <twh2898@vt.edu>
2021-08-04 01:01:22 -0400

    Removed old Parser and other unused code from Util

--- include/s3e/Support/Util.hpp
+++ include/s3e/Support/Util.hpp
@@ -2,15 +2,8 @@
 
 #include <fmt/ostream.h>
 
-#include <SFML/OpenGL.hpp>
-#include <cmath>
 #include <fstream>
 #include <glm/glm.hpp>
-#include <memory>
-#include <stdexcept>
-#include <string>
-
-#include "log.hpp"
 
 template<typename OStream>
 OStream & operator<<(OStream & os, const glm::vec2 & vec);
@@ -24,44 +17,3 @@ template<typename OStream>
 OStream & operator<<(OStream & os, const glm::mat3 & mat);
 template<typename OStream>
 OStream & operator<<(OStream & os, const glm::mat4 & mat);
-
-namespace Tom::s3e {
-
-    glm::mat4 matFromVecs(glm::vec3 pos,
-                          glm::vec3 rot,
-                          glm::vec3 scale = glm::vec3(1));
-
-    bool strStartsWithChar(char pre, const std::string & str);
-
-    bool strStartsWithStr(const std::string & pre, const std::string & str);
-
-    class Parser {
-        std::ifstream fin;
-
-    public:
-        using Ptr = std::shared_ptr<Parser>;
-        using ConstPtr = std::shared_ptr<const Parser>;
-
-        Parser(void);
-        Parser(const std::string & path);
-        ~Parser();
-
-        bool open(const std::string & path);
-
-        void close(void);
-
-        bool is_open(void) const;
-
-        bool eof(void) const;
-
-        std::string readToChar(char delim);
-
-        std::string readLine();
-
-        void rewind();
-
-        void rewind(int n);
-
-        static Ptr create(const std::string & path);
-    };
-}
--- src/Support/Util.cpp
+++ src/Support/Util.cpp
@@ -2,13 +2,6 @@
 
 #include <fmt/format.h>
 
-#include <cctype>
-#include <cstring>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
-#include <glm/gtx/transform.hpp>
-
 template<typename OStream>
 OStream & operator<<(OStream & os, const glm::vec2 & vec) {
     return os << fmt::format("[{}, {}]", vec[0], vec[1]);
@@ -37,103 +30,4 @@ OStream & operator<<(OStream & os, const glm::mat3 & mat) {
 template<typename OStream>
 OStream & operator<<(OStream & os, const glm::mat4 & mat) {
     return os << fmt::format("[{}, {}, {}, {}]", mat[0], mat[1], mat[2], mat[3]);
-}
-
-namespace Tom::s3e {
-    bool strStartsWithChar(char pre, const std::string & str) {
-        return !str.empty() && str[0] == pre;
-    }
-
-    bool strStartsWithStr(const std::string & pre, const std::string & str) {
-        return str.rfind(pre, 0) == 0;
-    }
-
-    glm::mat4 matFromVecs(glm::vec3 pos, glm::vec3 rot, glm::vec3 scale) {
-        glm::mat4 mpos = glm::translate(glm::mat4(1), pos);
-        glm::quat qrot(rot);
-        glm::mat4 mrot = glm::toMat4(qrot);
-        glm::mat4 msize = glm::scale(glm::mat4(1), scale);
-        return mpos * mrot * msize;
-    }
-}
-
-namespace Tom::s3e {
-
-    static int seekChar(std::ifstream & fin, char find) {
-        char c;
-        while (fin >> std::noskipws >> c && c != find)
-            ;
-        return c;
-    }
-
-    static int lengthToDelim(std::ifstream & fin, char delim) {
-        long begin = fin.tellg();
-        int c = seekChar(fin, '\n');
-        long end = fin.tellg();
-        fin.seekg(begin);
-        return end - begin;
-    }
-
-    Parser::Parser() {}
-
-    Parser::Parser(const std::string & path) {
-        open(path);
-    }
-
-    Parser::~Parser() {
-        close();
-    }
-
-    bool Parser::open(const std::string & path) {
-        fin.open(path);
-        return fin.is_open();
-    }
-
-    void Parser::close() {
-        fin.close();
-    }
-
-    bool Parser::is_open() const {
-        return fin.is_open();
-    }
-
-    bool Parser::eof() const {
-        return !fin;
-        // return !is_open() || fin.eof();
-    }
-
-    std::string Parser::readToChar(char delim) {
-        int len = lengthToDelim(fin, delim);
-        if (len < 1)
-            return std::string();
-
-        std::string buff(len, ' ');
-        fin.read(&buff[0], len);
-        buff.resize(buff.size() - 1); // Hacky way to remove \n
-
-        if (!fin) {
-            return std::string();
-        }
-
-        return buff;
-    }
-
-    std::string Parser::readLine() {
-        return readToChar('\n');
-    }
-
-    void Parser::rewind() {
-        fin.clear();
-        fin.seekg(0);
-    }
-
-    void Parser::rewind(int n) {
-        fin.clear();
-        fin.seekg(-n, std::ios::cur);
-    }
-
-    Parser::Ptr Parser::create(const std::string & path) {
-        auto parser = std::make_shared<Parser>(path);
-        return parser;
-    }
 }
