d0a3636ba3a3b4faafdb81c44d3f27404048c7b3
Thomas Harrison <twh2898@vt.edu>
2021-08-08 21:27:46 -0400

    Coppied workspace to examples/scenes, updated workspace CMakeLists.txt

---
+++ examples/scenes/Game.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <algorithm>
+#include <dispatch.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <random>
+#include <s3e.hpp>
+#include <thread>
+#include <vector>
+using namespace Tom::s3e;
+
+class Game : public GameBase {
+    ResourceManager resManager;
+    FPSDisplay::Ptr fps;
+    Scene::Ptr scene;
+    Scene::Ptr otherScene;
+    LocalDispatchQueue localTaskQueue;
+    DispatchQueue taskQueue;
+
+public:
+    Game(const sf::String & resPath);
+    virtual ~Game();
+
+    bool onCreate(void) override;
+    void onDestroy(void) override;
+
+    void onKeyPressed(const sf::Event::KeyEvent & e) override;
+    void onKeyReleased(const sf::Event::KeyEvent & e) override;
+    void onMouseMove(const sf::Event::MouseMoveEvent & e) override;
+    void onMouseDown(const sf::Event::MouseButtonEvent & e) override;
+    void onMouseUp(const sf::Event::MouseButtonEvent & e) override;
+    void onResized(const sf::Event::SizeEvent & e) override;
+
+    void onUpdate(const sf::Time & delta) override;
+    void onDraw(void) const override;
+};
