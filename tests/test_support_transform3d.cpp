356ba73d8b52b4d3021eb113ca27b4178e16492e
Thomas Harrison <twh2898@vt.edu>
2021-10-07 20:49:49 -0400

    Added placeholder tests for Transform3d and WavefrontParser

---
+++ tests/test_support_transform3d.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include <s3e/Support/Transform3d.hpp>
+
+namespace {
+    // TODO: Test Transform3d
+    /*
+    Test Plan:
+
+    - Defaults after constructor
+    - Values after copy / move constructor / assign opperator overload
+    - Relative move / rotate / scale
+    - Setters for position / rotation / scale
+
+    Check toMatrix when checking other fields.
+
+    Getters are implicitly tested.
+    */
+}
