01e19f94f3506cffccfc5bd44e08af07f0d0ef5d
Thomas Harrison <twh2898@vt.edu>
2021-10-23 19:42:15 -0400

    Remvoed ostream formatters for glm types

--- include/singe/Support/Util.hpp
+++ include/singe/Support/Util.hpp
@@ -1,24 +1,7 @@
 #pragma once
 
-#include <fmt/ostream.h>
-
-#include <fstream>
-#include <glm/glm.hpp>
 #include <string>
 #include <vector>
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec2 & vec);
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec3 & vec);
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec4 & vec);
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat2 & mat);
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat3 & mat);
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat4 & mat);
 
 /**
  * Split a string based on a delimeter.
--- src/Support/Util.cpp
+++ src/Support/Util.cpp
@@ -1,38 +1,6 @@
 #include "singe/Support/Util.hpp"
 
-#include <fmt/format.h>
-
 #include <sstream>
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec2 & vec) {
-    return os << fmt::format("[{}, {}]", vec[0], vec[1]);
-}
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec3 & vec) {
-    return os << fmt::format("[{}, {}, {}]", vec[0], vec[1], vec[2]);
-}
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::vec4 & vec) {
-    return os << fmt::format("[{}, {}, {}, {}]", vec[0], vec[1], vec[2], vec[3]);
-}
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat2 & mat) {
-    return os << fmt::format("[{}, {}]", mat[0], mat[1]);
-}
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat3 & mat) {
-    return os << fmt::format("[{}, {}, {}]", mat[0], mat[1], mat[2]);
-}
-
-template<typename OStream>
-OStream & operator<<(OStream & os, const glm::mat4 & mat) {
-    return os << fmt::format("[{}, {}, {}, {}]", mat[0], mat[1], mat[2], mat[3]);
-}
 
 std::vector<std::string> splitString(const std::string & str, char delim) {
     std::vector<std::string> parts;
