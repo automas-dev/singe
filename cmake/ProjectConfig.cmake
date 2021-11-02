184b817ed36ecf91fa2902b2ab314fcd1e5faf48
Thomas Harrison <twh2898@vt.edu>
2021-11-01 22:54:02 -0400

    Fixed cmake/ProjectConfig.cmake not taking project name, added dependencies to cmake/ProjectConfig.cmake

--- cmake/ProjectConfig.cmake
+++ cmake/ProjectConfig.cmake
@@ -1,9 +1,12 @@
 include(CMakeFindDependencyMacro)
 
-# TODO: capture @var@ config variables
+find_dependency(Threads REQUIRED)
+find_dependency(SFML 2.5 REQUIRED)
+find_dependency(GLEW REQUIRED)
+find_dependency(OpenGL REQUIRED)
+find_dependency(glm REQUIRED)
+find_dependency(fmt REQUIRED)
+find_dependency(spdlog REQUIRED)
+find_dependency(Bullet REQUIRED)
 
-find_dependency(fmtlib REQUIRED)
-
-# TODO: extra steps
-
-include("${CMAKE_CURRENT_LIST_DIR}/${PROJECT_NAME}Targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake")
