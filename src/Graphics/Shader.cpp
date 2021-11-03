b5cb1f906d01127455e563fdc10c174d6422752b
Thomas Harrison <twh2898@vt.edu>
2021-11-02 22:27:10 -0400

    Removed model uniform from default shader

--- src/Graphics/Shader.cpp
+++ src/Graphics/Shader.cpp
@@ -289,10 +289,9 @@ out vec3 FragPos;
 out vec3 FragNorm;
 out vec2 FragTex;
 uniform mat4 mvp;
-uniform mat4 model;
 void main() {
-    gl_Position = mvp * model * vec4(aPos, 1.0);
-    FragPos = vec3(model * vec4(aPos, 1.0));
+    gl_Position = mvp * vec4(aPos, 1.0);
+    FragPos = vec3(mvp * vec4(aPos, 1.0));
     FragNorm = aNorm;
     FragTex = aTex;
 })";
