d12fb94108f3e0cacdfcf728edb0452c270d6f7a
Thomas Harrison <twh2898@vt.edu>
2022-01-03 02:48:22 -0500

    Removed code from old project structure

--- include/singe/Graphics.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "glpp/FrameBuffer.hpp"
-#include "glpp/Shader.hpp"
-#include "glpp/Texture.hpp"
-#include "glpp/VBO.hpp"
--- src/Support/log.cpp
+++
@@ -1,6 +0,0 @@
-#include "singe/Support/log.hpp"
-
-namespace singe::Logging {
-    Logger::Ptr Graphics = std::make_shared<Logger>("Graphics");
-    Logger::Ptr Core = std::make_shared<Logger>("Core");
-}
