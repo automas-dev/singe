65bd1c6a203c94bee0838d49c9d6637f28e2110f
Thomas Harrison <twh2898@vt.edu>
2022-11-27 00:06:44 -0500

    Added missing implementation of Material

--- src/singe-graphics/src/Material.cpp
+++ src/singe-graphics/src/Material.cpp
@@ -7,9 +7,27 @@ namespace singe {
 
     Material::Material() {}
 
-    Material::Material(Material && other) {}
+    Material::Material(Material && other)
+        : name(std::move(other.name)),
+          ambient(std::move(other.ambient)),
+          diffuse(std::move(other.diffuse)),
+          specular(std::move(other.specular)),
+          specExp(other.specExp),
+          alpha(other.alpha),
+          texture(std::move(other.texture)),
+          normalTexture(std::move(other.normalTexture)),
+          specularTexture(std::move(other.specularTexture)) {}
 
     Material & Material::operator=(Material && other) {
+        name = std::move(other.name);
+        ambient = std::move(other.ambient);
+        diffuse = std::move(other.diffuse);
+        specular = std::move(other.specular);
+        specExp = other.specExp;
+        alpha = other.alpha;
+        texture = std::move(other.texture);
+        normalTexture = std::move(other.normalTexture);
+        specularTexture = std::move(other.specularTexture);
         return *this;
     }
 
