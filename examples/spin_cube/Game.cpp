9893a2aafd7447a700a6de8f7ac8d5194728e3c3
Thomas Harrison <twh2898@vt.edu>
2023-05-13 22:44:08 -0400

    Update spin_cube

--- examples/spin_cube/Game.cpp
+++ examples/spin_cube/Game.cpp
@@ -14,11 +14,15 @@ Game::Game(Window & window)
 
     // TODO: load here
 
-    shared_ptr<Mesh> model;
+    shared_ptr<Scene> modelScene;
 
-    model = scene.models.emplace_back(res.loadModel("cube.obj"));
-    model->transform.move({0, 0, 3});
-    model->material->shader = shader;
+    modelScene = scene.addChild();
+    modelScene->models = res.loadModel("cube.obj");
+    modelScene->transform.move({0, 0, 3});
+
+    for (auto & s : scene.children) {
+        for (auto & m : s->models) m->material->shader = shader;
+    }
 
     // Load models / textures / scenes
     // No fancy render api, just each model can be drawn
@@ -33,7 +37,7 @@ void Game::onUpdate(const sf::Time & delta) {
     float s = delta.asSeconds();
     // TODO: update here
     scene.transform.rotateEuler({0, s * 0.5, 0});
-    scene.models[0]->transform.rotateEuler({0, -s, 0});
+    scene.children[0]->transform.rotateEuler({0, -s, 0});
 }
 
 inline void setupGl() {
