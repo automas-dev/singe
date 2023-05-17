74017f6f1e9ecdba282f6165d491c97760aff7a8
Thomas Harrison <twh2898@vt.edu>
2023-05-16 21:12:46 -0400

    Committing wip updates to Util

--- src/singe-support/include/singe/Support/Util.hpp
+++ src/singe-support/include/singe/Support/Util.hpp
@@ -1,27 +1,34 @@
 #pragma once
 
+#include <istream>
 #include <string>
 #include <vector>
 
-/**
- * Split a string based on a delimeter.
- *
- * @param str the string to split
- * @param delim the delimiter
- *
- * @return a vector of strings
- */
-std::vector<std::string> splitString(const std::string & str, char delim = ' ');
+namespace singe::util {
+    using std::istream;
+    using std::string;
+    using std::vector;
 
-class Tokenizer {
-    char token;
-    std::istream & stream;
-    const std::istream & end;
+    class Tokenizer {
+        char token;
+        istream & stream;
+        const istream & end;
 
-public:
-    Tokenizer(std::istream & begin, const std::istream & end, char token);
+    public:
+        Tokenizer(istream & begin, const istream & end, char token);
 
-    bool hasNext();
+        bool hasNext();
 
-    std::string next();
-};
+        string next();
+    };
+
+    /**
+     * Split a string based on a delimeter.
+     *
+     * @param str the string to split
+     * @param delim the delimiter
+     *
+     * @return a vector of strings
+     */
+    vector<string> splitString(const string & str, char delim = ' ');
+}
--- src/singe-support/src/Util.cpp
+++ src/singe-support/src/Util.cpp
@@ -2,7 +2,7 @@
 
 #include <sstream>
 
-std::vector<std::string> splitString(const std::string & str, char delim) {
+std::vector<std::string> splitString_old(const std::string & str, char delim) {
     std::vector<std::string> parts;
     std::stringstream ss(str);
     std::string part;
@@ -12,24 +12,42 @@ std::vector<std::string> splitString(const std::string & str, char delim) {
     return parts;
 }
 
-Tokenizer::Tokenizer(std::istream & begin, const std::istream & end, char token)
-    : stream(begin), end(end), token(token) {}
+namespace singe::util {
+    using std::stringstream;
 
-bool Tokenizer::hasNext() {
-    return stream.good();
-}
+    Tokenizer::Tokenizer(istream & begin, const istream & end, char token)
+        : stream(begin), end(end), token(token) {}
 
-std::string Tokenizer::next() {
-    std::string part;
-
-    while (hasNext()) {
-        char c = stream.get();
-
-        if (stream.eof())
-            break;
-
-        part.push_back(c);
+    bool Tokenizer::hasNext() {
+        return stream.good();
     }
 
-    return part;
+    string Tokenizer::next() {
+        string part;
+
+        while (hasNext()) {
+            char c = stream.get();
+
+            if (stream.eof())
+                break;
+
+            part.push_back(c);
+        }
+
+        return part;
+    }
+
+    vector<string> splitString(const string & str, char delim) {
+        vector<string> parts;
+        auto it = str.begin();
+        auto start = it;
+        while (it != str.end()) {
+            if (*it == delim) {
+                parts.emplace_back(start, it);
+                start = it;
+            }
+            it++;
+        }
+        return parts;
+    }
 }
